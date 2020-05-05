0e6221710e03f0be75bd1061bc8938331b676081
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-05-05 10:37:27 +0200

    Add files via upload

---
+++ jour02/job01/index.php
@@ -0,0 +1,19 @@
+<?php /* Job1 */
+
+$nombre= 0;
+
+while ($nombre <=1337)
+{
+    if ($nombre==42)
+    {
+echo "<u>"."<b>".$nombre." </b>"."</u>";
+echo "<br>"; 
+$nombre++;
+}
+else{
+    echo  $nombre;
+    echo "<br/>";
+    $nombre++;
+}
+}
+?>
---
+++ jour02/job02/index.php
@@ -0,0 +1,17 @@
+<?php /* Job2 */
+
+$nombre= 0;
+
+while ($nombre <=1337)
+{
+    if ($nombre==26 ||$nombre==37 ||$nombre==88 ||$nombre==1111 ||$nombre==3233)
+    {
+$nombre++;
+}
+else{
+    echo  $nombre;
+    echo "<br/>";
+    $nombre++;
+}
+}
+?>
