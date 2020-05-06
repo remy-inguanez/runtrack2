2e7eacccb0625f339bf7939bfd180a992b11c3fd
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-05-06 11:08:27 +0200

    Add files via upload

---
+++ jour03/job02/index.php
@@ -0,0 +1,11 @@
+
+<?php // Job2
+    $str= "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie." ; 
+    $i = 0 ; 
+    while ($i <= 76) 
+    {
+        echo $str[$i];
+        $i++;
+        $i++; 
+    }
+?>
