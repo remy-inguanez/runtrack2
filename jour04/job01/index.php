75001c7875500605e2a0a96e44b5c5a568289473
remy-inguanez <55872961+remy-inguanez@users.noreply.github.com>
2020-05-07 10:20:32 +0200

    Add files via upload

---
+++ jour04/job01/index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+
+<form method="get" action="index.php">
+   <input type="text" name="ecole" />
+   <input type="text" name="email" />
+   <input type="submit" value="ok" />
+</form>
+
+<?php 
+
+   var_dump($_GET);
+
+?>
+
+</html>
